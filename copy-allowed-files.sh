6ec4de94c8f973b8523bc1cd301f9a659c3e89be
Egor Vinogradov <egor@ostrovok.ru>
2012-12-09 18:24:39 +0400

    fix

--- copy-allowed-files.sh
+++ copy-allowed-files.sh
@@ -5,15 +5,15 @@ counter=0
 mkdir allowed-files;
 while read line
 do
-	for file in *
-	do
-		if [[ $file = $line ]]
-		then
-			echo 'Copied:' $file
-			cp $file allowed-files
-			counter=$(($counter+1));
-		fi
-	done
+    for file in *
+    do
+        if [[ $file = $line ]]
+        then
+            echo 'Copied:' $file
+            cp $file allowed-files
+            counter=$(($counter+1));
+        fi
+    done
 done < 'allowed.txt' # File containing names of allowed files
 echo 'Done: copied' $counter;
 
